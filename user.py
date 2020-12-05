dda5797641d21c63b46f61ee84099482812b0578
jasonczc <jasonczc@qq.com>
2020-12-05 21:14:23 +0800

    使用新方式登陆用户

--- user.py
+++ user.py
@@ -8,11 +8,20 @@ class MFD:
     def __init__(self):
         self.userName = ""
         self.password = ""
-        # UFD *user  该用户文件目录的指针
+        self.user = None  # 该用户文件目录的指针
 
 
 userID = -1  # 用户登录的ID号，值为-1时表示没有用户登录
 userTable = [MFD()] * config.MAX_USER
+
+
+# 寻找对应的user table文件 假如没有相应记录则返回None
+def find_user_table(username):
+    # 遍历当前的MFD
+    for i in userTable:
+        if i.userName != "" and i.userName == username:
+            return i
+    return None
 
 
 # 用户登陆指令
@@ -20,17 +29,17 @@ def login_user(args):
     if len(args) <= 2:
         return False
     print("- 正在验证")
-    # 遍历当前的MFD
-    for i in userTable:
-        if i.userName != "" and i.userName == args[1]:
-            if i.password == args[2]:
-                print("! 登陆成功")
-                return True
-            else:
-                print("! 登陆失败 密码错误")
-                return False
-    print("! 未找到该用户")
-    return False
+    table = find_user_table(args[1])
+    if table is None:
+        print("! 未找到该用户")
+        return False
+    if table.userName != "" and table.userName == args[1]:
+        if table.password == args[2]:
+            print("! 登陆成功")
+            return True
+        else:
+            print("! 登陆失败 密码错误")
+            return False
 
 
 # 用户注册指令
