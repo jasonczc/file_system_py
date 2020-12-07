64630442bc1596148ea9f5ebab34b3da80c8e8f5
jasonczc <jasonczc@qq.com>
2020-12-07 22:41:30 +0800

    update open & close command

--- disk.py
+++ disk.py
@@ -4,6 +4,10 @@ import user
 import command
 import time
 
+MODE_READ_ONLY = 0    # 只读
+MODE_WRITE_ONLY = 1   # 只写
+MODE_READ_WRITE = 2   # 读写
+
 
 # 存储节点
 class DiskBlockNode:
@@ -17,7 +21,7 @@ class DiskBlockNode:
 class UserOpenFile:
     def __init__(self):
         self.uid = 0
-        self.filename = ""
+        self.file_name = ""
         self.mode = 0
         self.rw_location = 0
         self.sid = 0
@@ -26,12 +30,11 @@ class UserOpenFile:
 class SystemOpenFile:
     def __init__(self):
         self.sid = 0
-        self.filename = ""
+        self.open_file = ""
         self.dic_item = None
         self.open_count = 0
 
 
-system_open_file_table: SystemOpenFile = None
 disk_block_link = None  # 空闲盘块链
 root = file.FileItem()  # 根目录结点
 user_table = user.userTable  # 用户表
@@ -64,6 +67,16 @@ def get_empty_node_size():
     return length
 
 
+# 获取链表大小
+def get_node_size(node):
+    length = 0
+    now = node
+    while now is not None:
+        now = now.next
+        length += 1
+    return length
+
+
 def list_block_info(args):
     now_node = disk_block_link
     while now_node.next is not None:
@@ -83,7 +96,7 @@ def search_dic_item(uri, now1=root):
             continue
         flag = False
         for j in now.dic:
-            if i == j.filename:
+            if i == j.file_name:
                 now = j
                 flag = True
         if not flag:
@@ -206,6 +219,22 @@ def get_free_disk(target_len):
     return head
 
 
+def get_file_node(path):
+    nowdict = path.split("/")
+    need = ""
+    node = root
+    for i in range(len(nowdict) - 1):
+        need += f"/{nowdict[i]}"
+    # print(need)
+    if need != "":
+        node = find_dic_item(need)
+        if node is None:
+            print("!cannot find path")
+            return None
+    name = nowdict[len(nowdict) - 1]
+    return find_file_in_node(node, name)
+
+
 def create(args):
     if len(args) != 2:
         return False
@@ -241,9 +270,79 @@ def create(args):
     return True
 
 
+def open_command(args):
+    if len(args) < 3:
+        return False
+    item = get_file_node(args[1])
+    if item is None:
+        print(f"cannot find path {args[1]}")
+        return False
+    mode = int(args[2])
+    mode_list = [MODE_READ_ONLY, MODE_READ_WRITE, MODE_WRITE_ONLY]
+    if mode not in mode_list:
+        print("unknown mode")
+        return False
+    sys_table = None
+    sid =  len(system_open_file_table)
+    for i in system_open_file_table:
+        if i.dic_item.file_name == args[1]:
+            sys_table = i
+            sid = i.sid
+    if sys_table is None:
+        sid = len(system_open_file_table)
+        sys_table = SystemOpenFile()
+        sys_table.sid = sid
+        sys_table.open_file = args[1]
+        sys_table.dic_item = item
+        system_open_file_table.append(sys_table)
+    sys_table.open_count += 1
+    uid = len(user_open_file_table)
+    usr_table = UserOpenFile()
+    usr_table.sid = sid
+    usr_table.uid = uid
+    usr_table.mode = mode
+    usr_table.file_name = args[1]
+    user_open_file_table.append(usr_table)
+    print(f"success uid={uid}")
+    return True
+
+
+def close_command(args):
+    if len(args) < 2:
+        return False
+    uid = int(args[1])
+    print(f"{uid} - {len(user_open_file_table)}")
+    if uid >= len(user_open_file_table):
+        print("unknown uid")
+        return False
+    usr_table = user_open_file_table[uid]
+    sid = usr_table.sid
+    sys_table = system_open_file_table[sid]
+    user_open_file_table.remove(usr_table)
+    print(user_open_file_table)
+    if sys_table.open_count > 1:
+        sys_table.open_count -= 1
+    else:
+        system_open_file_table.remove(sys_table)
+        print(system_open_file_table)
+        for i in system_open_file_table:
+            if i.sid > sid:
+                i.sid -= 1
+        for i in user_open_file_table:
+            if i.sid > sid:
+                i.sid -= 1
+    print(f"close uid {uid} successfully")
+    return True
+
+
 command.register_command("listDisk", list_block_info, "/listDisk")
 command.register_command("dir", dir, "/dir [dictionary]")
 command.register_command("ls", dir, "/ls [dictionary]")
 command.register_command("cd", cd, "/cd <path>")
 command.register_command("create", create, "/create <path>")
 command.register_command("mkdir", mkdir, "/mkdir <pathname>")
+command.register_command("open", open_command, "/open <pathname> <mode(0=readonly,1=writeonly,2=readwrite)>")
+command.register_command("close", close_command, "/close <uid>")
+command.register_command("read", mkdir, "/mkdir <pathname>")
+command.register_command("write", mkdir, "/mkdir <pathname>")
+
